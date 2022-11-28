1ed321bc63c1545a9f6ab8c16a9299f70a8665fe
Vitor Giovane <vitorgpalves@gmail.com>
2022-11-27 23:30:55 -0300

    feat: install and configure sidekiq

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module DockerizedRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Define de queue adapter
+    config.active_job.queue_adapter = :sidekiq
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
